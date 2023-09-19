bf45d90d3d29b77995a4b64efddccd0d33242bef
RotoRadarRiemer <riemerjoshua@gmail.com>
2023-09-19 12:43:54 -0500

    submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
-import {puppyList} from './data.js'
-import './App.css'
-import { useState } from 'react'
+import { puppyList } from './data.js';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log("puppyList: ", puppyList);
 
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log(featuredPup);
+
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      <div className="puppy-list">
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={() => {
+              console.log("puppy id: ", puppy.id);
+              setFeatPupId(puppy.id);
+            }} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+      }  
 
-
-export default App
+export default App;
